e2773c95f616b5d9133aff064e1103f9a3f7af52
radim567 <87022843+radim567@users.noreply.github.com>
2022-02-11 00:45:59 +0100

    Update RecipeCard.js

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
-import placeholder from '../images/food-placeholder.png';
+// import placeholder from '../images/food-placeholder.png';
 
 const picturePath = '../images/meal-pics/';
 
 export function RecipeCard({ title, preparationTime, slug }) {
-  let pictureId = picturePath + slug + '.jpg';
+  let pictureFileName = picturePath + slug + '.jpg';
   return (
     <Card className="h-100">
       <Link to={`/recipe/${slug}`}>
-        <CardImg src={placeholder} />
+        <CardImg src={pictureFileName} />
       </Link>
       <CardBody>
         <CardTitle>{title}</CardTitle>
@@ -21,4 +21,3 @@ export function RecipeCard({ title, preparationTime, slug }) {
 }
 
 // <Card className="h-100"> - vsechny stejnou vysku
-// <CardImg src={pictureId} />;
